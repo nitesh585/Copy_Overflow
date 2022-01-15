7f0e392bc4671762db312b203121cc8e4bfdecf9
nitesh585 <niteshyadav585@gmail.com>
2022-01-15 13:10:43 +0530

    setup nextJS

--- models/user.models.js
+++
@@ -1,9 +0,0 @@
-module.exports = (mongoose) => {
-  var schema = mongoose.Schema({
-    name: String,
-    email: String,
-    password: String,
-  });
-
-  return mongoose.model("user", schema);
-};
